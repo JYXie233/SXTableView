0fb9b00fd8d59d880844ab90fcb2869fcec67adc
Tom <344924008@qq.com>
2019-04-24 01:11:13 +0800

    init

---
+++ table/src/main/java/com/xjy/table/SimpleTableDataSource.kt
@@ -0,0 +1,60 @@
+package com.xjy.table
+
+import android.content.res.Resources
+import android.widget.TextView
+
+class SimpleTableDataSource : TableDataSource {
+    override fun configTitle(textView: TextView) {
+
+    }
+
+    override fun title(): String {
+        return "表格"
+    }
+
+    override fun numOfColumn(): Int {
+        return 5
+    }
+
+    override fun numOfRow(): Int {
+        return 6
+    }
+
+    override fun configCell(textView: TextView, rowIndex: Int, columnIndex: Int) {
+        textView.text = "$rowIndex-$columnIndex"
+        if (rowIndex == 3 && columnIndex == 1){
+            textView.text = "很像很奥古斯丁噶岁的嘎斯U盾干哈岁的哈斯"
+        }
+
+    }
+
+    override fun titleOfColumn(columnIndex: Int): String {
+        return "标题$columnIndex"
+    }
+
+    override fun widthOfCell(columnIndex: Int): Int {
+        return if (columnIndex == 2) -1 else 160
+//        return true
+    }
+
+    override fun idOfCell(rowIndex: Int, columnIndex: Int): String {
+        if (columnIndex == 0) {
+            if (rowIndex < 3) {
+                return "aaaa"
+            }
+        }
+        if (columnIndex == 4){
+            if (rowIndex < 5){
+                return "bbbb"
+            }
+        }
+        if (rowIndex == 5){
+            if (columnIndex < 2){
+                return "cccc"
+            }
+        }
+        return super.idOfCell(rowIndex, columnIndex)
+    }
+
+}
+
---
+++ table/src/main/java/com/xjy/table/Size.kt
@@ -0,0 +1,3 @@
+package com.xjy.table
+
+data class Size(val width:Int, val height: Int)
